2f26b28670ec6354c9c49192c6235c98b9dd4f74
Nina <nxtang789@gmail.com>
2020-12-27 17:22:14 -0500

    authentication and logging in

--- app2/app.py
+++ app2/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'boston'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda i: i['name'] == name, items), None)
         return {'item' : item}, 200 if item else 404
@@ -19,6 +26,19 @@ class Item(Resource):
         item = {'name':name, 'price' : data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+
+    def put(slef, name):
+        data = request.get_json()
+        item = next(filter(lambda x:x['name'] == name, items), None)
+        if item is None:
+            item = {'name':name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
 
 class ItemList(Resource):
     def get(self):
