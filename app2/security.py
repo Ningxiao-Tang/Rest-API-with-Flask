8801ef874f43790eb95953f14b10b8720c54a9d5
Nina <nxtang789@gmail.com>
2020-12-28 00:54:22 -0500

    writing users to db

--- app2/security.py
+++ app2/security.py
@@ -1,18 +1,11 @@
 from werkzeug.security import safe_str_cmp
 from user import User
-users = [
-    User(1, 'bob', 'asdf')
-]
-
-username_mapping = { u.username: u for u in users}
-
-userid_mapping = {u.id : u for u in users}
 
 def authenticate(username, password):
-    u = username_mapping.get(username, None)
+    u = User.find_by_username(username)
     if u and safe_str_cmp(u.password, password):
         return u
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id.get(user_id)
